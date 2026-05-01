af0155df31db637bdd1fc366396c185b2cc3c37b
kyle <kyle@lindsay.gr>
2026-05-01 21:22:47 +0100

    removed un-needed template code

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
+//import { useState } from "react";
+//import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
   return (
     <div>
       <main className="container">
-        <h1>SimpleWebGen</h1>
+        <h1>Simple<span className= "col-accent">Web</span>Gen</h1>
 
         <p className="description">SimpleWebGen is a lightweight, easy to use website generator perfect for creating responsive static web pages. Create good looking websites with no previous experience just by defining your content and pressing generate.</p>
 
